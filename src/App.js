e2dbc6dc024c078a736b2bc50e8c5bd4362d7a85
ervinkacar1991 <kacarervin@gmail.com>
2022-07-27 12:24:04 +0200

    Add task state finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (e) => {
+    setNewTask(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const addTask = () => {
+    setTodoList([...todoList, newTask]);
+  };
+
   return (
     <div className="App">
-      <header>
-        <h1>Ervin's Todo List</h1>
-      </header>
+      <div className="addTask">
+        <input onChange={handleChange} />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className="list">
+        {todoList.map((task) => {
+          return <h1>{task}</h1>;
+        })}
+      </div>
     </div>
   );
 }
